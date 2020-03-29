82841d54ecc511f8f45476fac1aa9f9c13bc3af1
张天赐 <472546172@qq.com>
2020-03-29 20:31:17 +0800

    添加自定义异常并使用

---
+++ src/main/java/priv/asura/wechat_service/global/ServiceException.java
@@ -0,0 +1,11 @@
+package priv.asura.wechat_service.global;
+
+public class ServiceException extends Exception {
+    public ServiceException(String message) {
+        super(message);
+    }
+
+    public ServiceException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
