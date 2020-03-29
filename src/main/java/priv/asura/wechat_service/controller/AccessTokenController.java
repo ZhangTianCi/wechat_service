1ac983371fa8c68b860a1f80444da91f3bf694ca
张天赐 <472546172@qq.com>
2020-03-29 20:31:46 +0800

    AccessToken使用代理方式实现

--- src/main/java/priv/asura/wechat_service/controller/AccessTokenController.java
+++ src/main/java/priv/asura/wechat_service/controller/AccessTokenController.java
@@ -1,25 +1,36 @@
 package priv.asura.wechat_service.controller;
 
-import priv.asura.wechat_service.global.ApiResult;
-import priv.asura.wechat_service.service.AccessTokenService;
 import org.springframework.beans.factory.annotation.Autowired;
+import priv.asura.wechat_service.global.ApiResult;
+import priv.asura.wechat_service.global.ServiceException;
+import priv.asura.wechat_service.model.Account;
+import priv.asura.wechat_service.service.AccessTokenProxyService;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
 
 @RestController
 @RequestMapping("/access_token")
 public class AccessTokenController {
+    final
+    HttpServletRequest request;
+    final AccessTokenProxyService service;
 
-    final AccessTokenService service;
-
-    public AccessTokenController(AccessTokenService service) {
+    public AccessTokenController(HttpServletRequest request, AccessTokenProxyService service) {
+        this.request = request;
         this.service = service;
     }
 
     @RequestMapping(value = {"/", "get"})
-    public ApiResult get() {
+    public ApiResult get() throws ServiceException {
+        // 此处需要校验
+        Account account = new Account() {{
+            setWeChatAppId(request.getHeader("Client-Id"));
+            setWeChatAppSecret(request.getHeader("Client-Secret"));
+        }};
 
-        return ApiResult.success();
+        return ApiResult.success(service.getInstance(account).get());
     }
 
     @RequestMapping("refresh")
--- src/main/java/priv/asura/wechat_service/service/AccessTokenService.java
+++ src/main/java/priv/asura/wechat_service/service/AccessTokenService.java
@@ -1,11 +1,66 @@
 package priv.asura.wechat_service.service;
 
-import org.springframework.stereotype.Service;
+import lombok.extern.slf4j.Slf4j;
+import priv.asura.wechat_service.global.ServiceException;
+import priv.asura.wechat_service.model.Account;
+import priv.asura.wechat_service.model.wechat.AccessToken;
+import priv.asura.wechat_service.utils.HttpUtil;
 
-@Service
-public class AccessTokenService {
+import java.io.File;
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.rmi.ServerException;
 
-    public String get(String clientId, String clientSecret) {
-        return clientId + clientSecret;
+@Slf4j
+public class AccessTokenService extends DataService {
+
+    private Account account;
+
+    public AccessTokenService(Account account) {
+        this.account = account;
+    }
+
+    public String get() throws ServiceException {
+        // 需要单次失败检测
+        AccessToken accessToken = null;
+        try {
+            accessToken = getFromData();
+        } catch (Exception e) {
+            refresh();
+            log.warn("首次获取出错。" + e);
+        }
+        if (accessToken == null) {
+            throw new ServiceException("经过首次容错后，依然没有获取到AccessToken");
+        } else if (!accessToken.isValid()) {
+            return getFromData().getAccessToken();
+        } else {
+            return accessToken.getAccessToken();
+        }
+    }
+
+    private AccessToken getFromData() throws ServiceException {
+        return this.readData(AccessToken.class);
+    }
+
+    public void refresh() throws ServiceException {
+        String uri = "https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential";
+        uri += "&appid=" + account.getWeChatAppId();
+        uri += "&secret=" + account.getWeChatAppSecret();
+        try {
+            AccessToken accessToken = new HttpUtil(HttpUtil.Method.GET).setUri(uri).send(AccessToken.class);
+            if (accessToken.isValid()) {
+                writeData(accessToken);
+            } else {
+                throw new ServiceException(accessToken.getErrorMessage());
+            }
+        } catch (Exception ex) {
+            log.error("通过接口请求AccessToken失败。", ex);
+            throw new ServiceException("通过接口请求AccessToken失败。", ex);
+        }
+    }
+
+    @Override
+    public String getFilePath() {
+        return "access_token" + File.separator + account + ".data";
     }
 }
